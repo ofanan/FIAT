9a5f05474a2c904e78ba29dd4364d247ebf962d9
ofanan <ofanan@gmail.com>
2023-03-31 21:16:26 +0300

    Working version of greedy and opt alg for homo parallel_accs_strat.

--- src/parallel_accs_strat.py
+++ src/parallel_accs_strat.py
@@ -2,10 +2,13 @@ import itertools
 from itertools import combinations
 from numpy import infty
 
-q       = 0.5 # prob' of failure
-missp   = 100
-numRsrc = 5
-numUsedTimeSlots = 5  # number of balls
+def calcSolCost (sol):
+    sol = sol.copy() # as sol is modified by this func, we copy it to a local var
+    solCost = sol[-1] + q**sol[-1] * missp
+    sol.pop()
+    for sol_t in reversed (sol):
+        solCost = sol_t + q**sol_t * solCost
+    return solCost 
 
 def exhaustSearchForOptSol ():
     for numUsedTimeSlots in range(1, numRsrc+1): # iterate over all possible combinations
@@ -21,16 +24,45 @@ def exhaustSearchForOptSol ():
                 minCost = solCost 
     print ('optSol={}, minCost={}' .format (optSol, minCost))
 
-def calcSolCost (sol):
-    solCost = sol[-1] + q**sol[-1] * missp
-    sol.pop()
-    for sol_t in reversed (sol):
-        solCost = sol_t + q**sol_t * solCost
-    return solCost 
+def greedySearchForOptSol ():
+    curStepSol = [0]
+    optSol     = curStepSol
+    minCost    = calcSolCost(curStepSol)
+    print ('suggestedSol={}, cost={}' .format (optSol, minCost))
 
-# def exhaustSearchForOptSol (solSize = 2):
-#     if (solSize> numRsrc):
-#         MyConfig.error ('solSize should be <= num o frsrcs, which is {}' .format (numRsrc))
+    while (sum (curStepSol) < numRsrc):
+        if (curStepSol==[0]):
+            curStepSol = [1]
+            solCost    = calcSolCost(curStepSol)
+            if (solCost < minCost):
+                minCost = solCost
+                optSol  = curStepSol
+                print ('suggestedSol={}, cost={}' .format (optSol, solCost))
+                continue
+            else:
+                MyConfig.error ('cheapest sol is [0]') 
+        for slot in range (len(curStepSol)):
+            suggestedSol        = curStepSol.copy ()
+            suggestedSol[slot] += 1
+            solCost             = calcSolCost(suggestedSol)
+            print ('suggestedSol={}, cost={}' .format (suggestedSol, solCost))
+            if (solCost < minCost):
+                minCost = solCost
+                optSol  = suggestedSol
+        suggestedSol = curStepSol.copy ()
+        suggestedSol.append (1)
+        solCost      = calcSolCost(suggestedSol)
+        print ('suggestedSol={}, cost={}' .format (suggestedSol, solCost))
+        if (solCost < minCost):
+            minCost = solCost
+            optSol  = suggestedSol
+        curStepSol = optSol.copy ()
 
 
+q       = 0.2 # prob' of failure
+missp   = 10
+numRsrc = 5
+numUsedTimeSlots = 5  # number of balls
+
+greedySearchForOptSol ()
 exhaustSearchForOptSol ()

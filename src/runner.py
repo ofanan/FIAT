fd7402904fa3e8926019c1c755e57fe4a7162ff4
ofanan <ofanan@gmail.com>
2023-07-02 20:44:03 +0300

    fe

--- src/runner.py
+++ src/runner.py
@@ -14,17 +14,17 @@ from   tictoc import tic, toc
 def main ():
     min_feasible_uInterval = 10
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
-    for trace in ['IBM1', 'Twitter45', 'F1']: #, 'IBM', 'Scarab','F2', 'Wiki']: #['Twitter', 'IBM', 'Scarab','F2', 'Wiki', 'Twitter45', 'F1', 'IBM1'      
+    for trace in ['IBM7', 'Scarab','F2', 'Wiki']: #['Twitter', 'IBM', 'Scarab','F2', 'Wiki', 'Twitter45', 'F1', 'IBM1'      
         for DS_size in [4000]: #[4000, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) 
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req) 
-            for mode in ['opt']:
+            for mode in ['salsa2']:
                 for missp in [30, 100, 300]: #[10, 30, 100, 300]:
                     tic()
                     sm = sim.DistCacheSimulator(
                         res_file_name           = f'salsa2_minFU{min_feasible_uInterval}' if mode=='salsa2' else mode,
                         EWMA_alpha_mr0          = 0.85, 
-                        EWMA_alpha_mr1          = 0.85, 
+                        EWMA_alpha_mr1          = 0.25, 
                         trace_name              = trace,
                         mode                    = mode,
                         req_df                  = requests,

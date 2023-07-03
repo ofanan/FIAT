c3076ba021f339b37640347b9f5a5bcf19c3075d
ofanan <ofanan@gmail.com>
2023-07-03 15:30:18 +0300

    grrg

--- src/runner.py
+++ src/runner.py
@@ -16,11 +16,11 @@ def main ():
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
     # for trace in ['Twitter45', 'F2', 'IBM7']: 
     for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
-        for DS_size in [4000]: #[4000, 16000, 64000]:
+        for DS_size in [64000]: #[4000, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) 
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req) 
             for mode in ['salsa2']:
-                for missp in [30, 300, 10]: #[10, 30, 100, 300]:
+                for missp in [30]: #[10, 30, 100, 300]:
                     tic()
                     sm = sim.DistCacheSimulator(
                         res_file_name           = f'salsa2_minFU{min_feasible_uInterval}' if mode=='salsa2' else mode,

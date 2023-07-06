68207b3cb517fe7cb12aed0692f877cdef31af95
ofanan <ofanan@gmail.com>
2023-07-06 16:48:02 +0300

    rge

--- src/runner.py
+++ src/runner.py
@@ -13,12 +13,12 @@ from   tictoc import tic, toc
 
 def main ():
     min_feasible_uInterval = 10
-    for num_of_DSs in range (1, 9):
-        DS_cost = calc_DS_cost (num_of_DSs=num_of_DSs, use_homo_DS_cost=True)
+    for num_of_DSs in range (3, 4): #(1, 9):
+        DS_cost = calc_DS_cost (num_of_DSs=num_of_DSs, use_homo_DS_cost=False)
         # for trace in ['F1', 'IBM1', 'Twitter45']:
-        for trace in ['F1', 'F2', 'IBM7', 'IBM1']:     
+        # for trace in ['F1', 'F2', 'IBM7', 'IBM1']:     
         # for trace in ['Scarab', 'Wiki', 'Twitter17', 'Twitter45']:       # for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
-        # for trace in ['F1']:        
+        for trace in ['F1']:        
         # for trace in ['F2']: 
         # for trace in ['IBM1']: 
         # for trace in ['IBM7']: 
@@ -26,11 +26,11 @@ def main ():
         # for trace in ['Twitter45']:
         # for trace in ['Scarab']:       
         # for trace in ['Wiki']:       
-            for DS_size in [10000]: #[, 16000, 64000]:
+            for DS_size in [64000]: #[, 16000, 64000]:
                 max_num_of_req = MyConfig.calc_num_of_req (trace) 
                 requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
-                for mode in ['opt']:
-                    for missp in [100]: #[10, 30, 100, 300]:
+                for mode in ['salsa2']:
+                    for missp in [30, 300]: #[10, 30, 100, 300]:
                         tic()
                         sm = sim.DistCacheSimulator(
                             res_file_name           = f'{mode}_HPC',
@@ -45,7 +45,7 @@ def main ():
                             min_uInterval           = DS_size/10,
                             re_init_after_each_ad   = False,
                             min_feasible_uInterval  = min_feasible_uInterval,
-                            uInterval_factor        = 2 if mode.startswith('salsa') else 1,
+                            uInterval_factor        = 32 if mode.startswith('salsa') else 1,
                             verbose                 = [MyConfig.VERBOSE_RES])
                         sm.run_simulator(interval_between_mid_reports=max_num_of_req/10)
                         toc()

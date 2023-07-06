5f907e65b07fe8a4db726d965816a27fd93eea85
ofanan <ofanan@gmail.com>
2023-07-06 16:11:09 +0300

    reg

--- src/runner.py
+++ src/runner.py
@@ -28,7 +28,7 @@ def main ():
         for DS_size in [4000]: #[, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) 
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
-            for mode in ['salsa2']:
+            for mode in ['opt']:
                 for missp in [10]: #[10, 30, 100, 300]:
                     tic()
                     sm = sim.DistCacheSimulator(

1a3b572fa965214f73f5ea525b9fdb61c602b190
ofanan <ofanan@gmail.com>
2023-07-16 16:15:08 +0300

    regrg

--- src/runner.py
+++ src/runner.py
@@ -15,7 +15,7 @@ import mod_pylru
 def main ():
     min_feasible_uInterval = 10
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
-    # for trace in ['Wiki', 'Scarab', 'F1', 'F2']:
+    for trace in ['Wiki', 'Scarab', 'F1', 'F2']:
     # for trace in ['IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
     # for trace in ['IBM1', 'Wiki', 'F1', 'Twitter45']:       # for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
     # for trace in ['Wiki']:        
@@ -23,14 +23,14 @@ def main ():
     # for trace in ['IBM1']: 
     # for trace in ['IBM7']: 
     # for trace in ['Twitter17']:
-    for trace in ['Twitter45']:
+    # for trace in ['Twitter45']:
     # for trace in ['Scarab']:       
     # for trace in ['IBM7']:       
         for DS_size in [4000]: #[, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) 
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
             for mode in ['salsa2']:
-                for missp in [300]: #[10, 30, 100, 300]:
+                for missp in [30]: #[10, 30, 100, 300]:
                     tic()
                     sm = sim.DistCacheSimulator(
                         res_file_name           = f'{mode}_HPC',

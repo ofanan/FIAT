3cc1ec9d99923e2f12a52ff51e88713928a14024
ofanan <ofanan@gmail.com>
2023-07-20 16:33:33 +0300

    ger

--- src/runner.py
+++ src/runner.py
@@ -20,20 +20,20 @@ def run_hetro_costs_sim ():
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
     # for trace in ['Wiki', 'Scarab', 'F1', 'F2']: # , 'IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
     # for trace in ['IBM1', 'Wiki', 'F1', 'Twitter45']:       # for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
-    # for trace in ['Wiki']:        
+    for trace in ['Wiki']:        
     # for trace in ['Scarab']:       
     # for trace in ['F1']: 
     # for trace in ['F2']: 
     # for trace in ['IBM1']: 
     # for trace in ['IBM7']: 
     # for trace in ['Twitter17']:
-    for trace in ['Twitter45']:
+    # for trace in ['Twitter45']:
     # for trace in ['IBM7', 'Twitter17']: 
-        for DS_size in [16000]: #[, 16000, 64000]:
+        for DS_size in [64000]: #[, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) # 500000 #$$$$  
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
-            for mode in ['fnaa']:
-                for missp in [30, 300, 10]: #[10, 30, 100, 300]:
+            for mode in ['salsa2']:
+                for missp in [30]: #[10, 30, 100, 300]:
                     tic()
                     sm = sim.DistCacheSimulator(
                         # bpe                     = 10, #$$$

834c9db792a5b327bd0ec8e67d1d6f21068b3000
ofanan <ofanan@gmail.com>
2023-07-20 16:09:03 +0300

    rge

--- src/runner.py
+++ src/runner.py
@@ -22,13 +22,14 @@ def run_hetro_costs_sim ():
     # for trace in ['IBM1', 'Wiki', 'F1', 'Twitter45']:       # for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
     # for trace in ['Wiki']:        
     # for trace in ['Scarab']:       
-    for trace in ['F1']: 
+    # for trace in ['F1']: 
     # for trace in ['F2']: 
     # for trace in ['IBM1']: 
     # for trace in ['IBM7']: 
     # for trace in ['Twitter17']:
     # for trace in ['Twitter45']:
-        for DS_size in [16000]: #[, 16000, 64000]:
+    for trace in ['IBM7', 'Twitter17']: 
+        for DS_size in [4000]: #[, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) # 500000 #$$$$  
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
             for mode in ['fnaa']:
@@ -36,7 +37,7 @@ def run_hetro_costs_sim ():
                     tic()
                     sm = sim.DistCacheSimulator(
                         # bpe                     = 10, #$$$
-                        period_param            = 10, # length of "sync periods" of the indicator's scaling alg.
+                        period_param            = 5, # length of "sync periods" of the indicator's scaling alg.
                         res_file_name           = f'{mode}_HPC',
                         EWMA_alpha_mr0          = 0.85, 
                         EWMA_alpha_mr1          = 0.25, 

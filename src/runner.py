35a284bb98f0793d65c7794985489d874a0db85a
ofanan <ofanan@gmail.com>
2023-07-21 12:33:43 +0300

    erg

--- src/runner.py
+++ src/runner.py
@@ -20,16 +20,16 @@ def run_hetro_costs_sim ():
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
     # for trace in ['Wiki', 'Scarab', 'F1', 'F2']: # , 'IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
     # for trace in ['IBM1', 'Wiki', 'F1', 'Twitter45']:       # for trace in ['F1', 'IBM1', 'Scarab', 'Wiki', 'Twitter17']:       
-    # for trace in ['Wiki']:        
+    for trace in ['Wiki']:        
     # for trace in ['Scarab']:       
-    for trace in ['F1']: 
+    # for trace in ['F1']: 
     # for trace in ['F2']: 
     # for trace in ['IBM1']: 
     # for trace in ['IBM7']: 
     # for trace in ['Twitter17']:
     # for trace in ['Twitter45']:
     # for trace in ['IBM7', 'Twitter17']: 
-        for DS_size in [4000]: #[, 16000, 64000]:
+        for DS_size in [64000]: #[, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) # 500000 #$$$$  
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
             for mode in ['salsa2']:
@@ -205,3 +205,4 @@ if __name__ == '__main__':
         run_hetro_costs_sim ()
     except KeyboardInterrupt:
         print('Keyboard interrupt.')
+

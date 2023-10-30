0f6cb5308d1c45ddfd239a98ec3a1184c82aea0f
ofanan <ofanan@gmail.com>
2023-10-30 13:36:13 +0200

    rae

--- src/runner.py
+++ src/runner.py
@@ -49,7 +49,7 @@ def run_hetro_costs_sim ():
                         min_uInterval           = DS_size/10,
                         re_init_after_each_ad   = False,
                         min_feasible_uInterval  = min_feasible_uInterval,
-                        uInterval_factor        = 1.8 if mode.startswith('salsa') else 1,
+                        uInterval_factor        = 1 if mode.startswith('salsa') else 1,
                         verbose                 = [MyConfig.VERBOSE_RES, MyConfig.VERBOSE_FULL_RES],
                         # begin_log_mr_at_req_cnt = 74075,
                         ) # MyConfig.VERBOSE_RES, MyConfig.VERBOSE_FULL_RES, MyConfig.VERBOSE_DETAILED_LOG_MR

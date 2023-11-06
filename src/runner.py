a4298a23297d91951315c959335eeebd3eca90d4
ofanan <ofanan@gmail.com>
2023-11-06 15:59:07 +0200

    era

--- src/runner.py
+++ src/runner.py
@@ -19,17 +19,16 @@ def run_hetro_costs_sim ():
     min_feasible_uInterval = 10
     DS_cost = calc_DS_cost (num_of_DSs=3, use_homo_DS_cost=False)
     # for trace in ['Wiki', 'Scarab', 'F1', 'F2', 'IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
-    for trace in ['F1', 'F2', 'IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
     # for trace in ['F1', 'F2', 'IBM1', 'IBM7', 'Twitter17', 'Twitter45']:     
     # for trace in ['Wiki']:        
     # for trace in ['Scarab']:       
-    # for trace in ['F1']: 
+    for trace in ['F1']: 
     # for trace in ['F2']: 
     # for trace in ['IBM1']: 
     # for trace in ['IBM7']: 
     # for trace in ['Twitter17']:
     #for trace in ['Twitter45']:
-        for DS_size in [16000]: #[4000, 16000, 64000]:
+        for DS_size in [64000]: #[4000, 16000, 64000]:
             max_num_of_req = MyConfig.calc_num_of_req (trace) # 500000 #$$$$  
             requests = MyConfig.gen_requests (MyConfig.trace_csv_file_name[trace], max_num_of_req=max_num_of_req)  
             for mode in ['salsa_dep0']: #'salsa_dep0', 'fnaa'

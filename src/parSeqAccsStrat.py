9a64cce95a6a856697f76902d5ce105cd3b064fd
ofanan <ofanan@gmail.com>
2024-01-11 16:18:22 +0200

    Updated commit from home, inc. improved doc.

--- src/parSeqAccsStrat.py
+++ src/parSeqAccsStrat.py
@@ -53,16 +53,6 @@ class parSeqAccsStrat (object):
             self.greedySol         = sol.copy ()
             self.updatedGreedySol     = True
     
-    def dynProgAlg (self):
-        """
-        Finds a solution using dynamic programming.
-        The alg' first finds the best sols for each # of rsrcs when T=1 (can use a single slot). 
-        when inc' the # of slots to n+1, the algo' considers all the possible combinations of i rsrcs at time 0,
-        and (at most?) n+1-i rsrcs in the remainder slot.
-        , the algorithm greedily accesses one additional rsrc, in a way that minimizes the cost.
-        The alg' assumes that once it's not beneficial to add a rsrc, we've reached an optimal sol. 
-        Returns [greedySol, greedyCost], where greedySol is the solution vector, and greedyCost is its (expected) cost.  
-        """
     def greedyAlg (self):
         """
         Finds a solution using a greedy algorithm.
@@ -100,14 +90,17 @@ class parSeqAccsStrat (object):
 
 my_parSeqAccsStrat = parSeqAccsStrat ()
 resFile = open ('../res/parSeqAccsHomo.txt', 'w')
-printf (resFile, '// gSol, oSol are the greedy solution, and optimal solution, resp.\n\n')
-for q in [0.1*i for i in range (11)]:
+# printf (resFile, '// gSol, oSol are the greedy solution, and optimal solution, resp.\n\n')
+for q in [0.1*i for i in range (1, 10)]:
     for missp in [5, 10, 100, 500]:
-        for maxNumRsrc in range (1, 10):
-          for T in range (1, maxNumRsrc+1):
-            [greedySol, greedySolCost] = my_parSeqAccsStrat.greedyAlg ()
-            [optSol,    optSolCost   ] = my_parSeqAccsStrat.exhaustSearchForOptSol ()
-            if greedySolCost!=optSolCost:
-                print ('q={}, missp={}, R={}, T={}, greedySol={}, greedyCost={}, optSol={}, optCost={}' .format 
-                       (q, missp, maxNumRsrc, T, greedySol, greedySolCost, optSol, optSolCost))
-            printf (resFile, '{}\n' .format (optSol))
+        for T in range (1, 11):
+            for maxNumRsrc in range (3, 10):
+                if T>maxNumRsrc:
+                    continue
+                printf (resFile, '// R={}, T={}, q={:.01}, missp={}\n' .format (maxNumRsrc, T, q, missp))
+                [greedySol, greedySolCost] = my_parSeqAccsStrat.greedyAlg ()
+                [optSol,    optSolCost   ] = my_parSeqAccsStrat.exhaustSearchForOptSol ()
+                if greedySolCost!=optSolCost:
+                    print ('q={}, missp={}, R={}, T={}, greedySol={}, greedyCost={}, optSol={}, optCost={}' .format 
+                           (q, missp, maxNumRsrc, T, greedySol, greedySolCost, optSol, optSolCost))
+                printf (resFile, '{}\n' .format (optSol))

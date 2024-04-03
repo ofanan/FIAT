195392fda7f687cfc7ea3407b152832c84703903
ofanan <ofanan@gmail.com>
2024-04-03 08:36:48 +0300

    aerera

--- src/runner.py
+++ src/runner.py
@@ -58,8 +58,7 @@ def run_hetro_costs_sim ():
                         min_feasible_uInterval  = min_feasible_uInterval,
                         uInterval_factor        = 4 if mode.startswith('salsa') else 1,
                         verbose                 = verbose
-                        # begin_log_mr_at_req_cnt = 74075,
-                        ) # MyConfig.VERBOSE_RES, MyConfig.VERBOSE_FULL_RES, MyConfig.VERBOSE_DETAILED_LOG_MR
+                        ) 
                     sm.run_simulator(interval_between_mid_reports=max_num_of_req/10) 
                     toc()
         
